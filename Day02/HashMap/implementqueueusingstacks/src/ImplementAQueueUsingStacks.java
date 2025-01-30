ea689773391216290a845a9f05cd71dd1d9b3083
thakur-Yuvraj <yuvipatel224@gmail.com>
2025-01-30 15:55:10 +0530

    HashMap folder updated

--- Day02/HashMap/implementqueueusingstacks/src/ImplementAQueueUsingStacks.java
+++ Day02/HashMap/implementqueueusingstacks/src/ImplementAQueueUsingStacks.java
@@ -3,16 +3,31 @@
 //        Hint: Use one stack for enqueue and another stack for dequeue. Transfer elements between stacks as needed.
 
 
-import java.util.*;
 
 public class ImplementAQueueUsingStacks {
     public static void main(String[] args) {
-        MyQueue stk = new MyQueue();
-        var i = stk.hashCode();
-        Queue<Integer> que1 = new LinkedList<>() ;
-        que1.add(9);
-        que1.add(6);
-        que1.peek();
-        que1.remove();
+        // creating que object
+        MyQueue que2 = new MyQueue();
+
+        // trying to remove from empty que
+        que2.remove();
+
+        // adding element
+        que2.add(9);
+        que2.add(5);
+        que2.add(3);
+
+        // printing the front most val
+        System.out.println(que2.peek());
+        que2.remove();
+        System.out.println(que2.peek());
+
+        // checking if it is empty or not
+        if (que2.isEmpty()) {
+            System.out.println("Que is empty");
+
+        }else {
+            System.out.println("Que is not empty");
+        }
     }
 }
--- Day02/HashMap/implementqueueusingstacks/src/MyQueue.java
+++ Day02/HashMap/implementqueueusingstacks/src/MyQueue.java
@@ -1,13 +1,60 @@
-import java.util.Objects;
 import java.util.Stack;
 
+// using stack to implement que
 class MyQueue {
-    Stack<Objects> stk1 = new Stack<>();
-    Stack<Objects> stk2 = new Stack<>();
-
-    private int stk1Idx = 0;
-    private int stk2Idx = 0;
+    // making two stk object
+    Stack<Object> stk1 = new Stack<>();
+    Stack<Object> stk2 = new Stack<>();
 
 
+    // method to add variables
+    public void add(Object val) {
+        stk1.add(val);
+    }
+
+    // to get front most element
+    public Object peek() {
+        while(stk1.size() != 1) {
+            Object cur = stk1.peek();
+            stk2.add(cur);
+            stk1.pop();
+        }
+        Object top = stk1.peek();
+        while(!stk2.isEmpty()) {
+            Object cur = stk2.peek();
+            stk1.add(cur);
+            stk2.pop();
+        }
+        return top;
+    }
+
+    // removing the front element
+    public void remove() {
+        if (isEmpty()) {
+            System.out.println("Queue underflow");
+            return;
+        }
+        while(stk1.size() != 1) {
+            Object cur = stk1.peek();
+            stk2.add(cur);
+            stk1.pop();
+        }
+        stk1.pop();
+        while(!stk2.isEmpty()) {
+            Object cur = stk2.peek();
+            stk1.add(cur);
+            stk2.pop();
+        }
+    }
+
+    // function to return size of que
+    public int size() {
+        return stk1.size();
+    }
+
+    // function to check if que is empty or not
+    public boolean isEmpty() {
+        return stk1.isEmpty();
+    }
 
 }
